06a4e8e434bb8e89df3d6511506266ddb7019278
최동훈 <choidonghun@choedonghuns-MacBook-Pro.local>
2023-05-23 08:51:39 +0900

    update __self__

--- car.py
+++ car.py
@@ -1,13 +1,17 @@
 from pyfirmata import Arduino,util
 # import tensorflow
-import pandas
-import time
+# import pandas
 import os
 
 
 
 
-board = Arduino('/dev/cu.usbmodem21401')
+# board = Arduino('/dev/cu.usbmodem1201')
+# board = Arduino('/dev/cu.usbmodem21201')
+# board = Arduino('/dev/cu.usbmodem21401')
+board = Arduino('/dev/cu.usbmodem1401')
+
+
 it = util.Iterator(board)
 it.start()
 board.analog[0].enable_reporting()
@@ -15,7 +19,6 @@ board.analog[0].enable_reporting()
 # use ls /dev to found your arduino.
 # 컴터에 따라 뱌꿔요.
 
-print('connected to Arduino')
 
 # Set up Steering pins
 
@@ -40,15 +43,17 @@ steering_speed_pin.write(1)
 throttle_speed_pin.write(1)
 throttle_speed_pin1.write(1)
 
+print('connected to Arduino')
 class Car:
     def __init__(self):
         self.a = 1
         self.speed = 0
+        self.steeringAngleRaw = 0
         self.status = "Cooling Down :D"
+        self.batteryVoltage = 13.0
         
     def forward(self,speed):
         self.speed = speed
-        print("creeping forward")
         self.status = "creeping forward"
         throttle_speed_pin.write(speed)
         throttle_speed_pin1.write(speed)
@@ -59,7 +64,7 @@ class Car:
 
     def backward(self,speed):
         self.speed = -speed
-        print("backing up")
+        self.status = "backing up"
         throttle_speed_pin.write(speed)
         throttle_speed_pin1.write(speed)
         throttle_IN0.write(0)
@@ -68,16 +73,14 @@ class Car:
         throttle_IN11.write(1)
         
     def stop(self):
-        print("Dispowering motor")
+        self.status = "Dispowering rear motors"
         throttle_speed_pin.write(0)
         throttle_speed_pin1.write(0)
-        print("Motor dispowered")
-        print("Stopping motor")
         throttle_IN0.write(0)
         throttle_IN1.write(0)
         throttle_IN01.write(0)
         throttle_IN11.write(0)
-        print("Motor stoped")
+        self.status = "Motor stoped"
 
     def left(self):
         ctd = steering_sig_pin.read()
@@ -86,6 +89,7 @@ class Car:
             steering_IN1.write(0)
         steering_IN0.write(0)
         steering_IN1.write(0)
+        self.steeringAngleRaw = steering_sig_pin.read()
 
     def right(self):
         ctd = steering_sig_pin.read()
@@ -94,17 +98,70 @@ class Car:
             steering_IN1.write(1)
         steering_IN0.write(0)
         steering_IN1.write(0)
+        self.steeringAngleRaw = steering_sig_pin.read()
         
     def getInfo(self):
         
         # sendInfo
         info = {
-                'steeringAngleRaw':steering_sig_pin.read(),
-                'steeringAngleDeg':360 * (steering_sig_pin.read()-0.5),
-                'motorSpeed':self.speed,
-                'batteryStatus': 0.82, #hardcoded data.
-                'generalStatus': self.status
+                "steeringAngleRaw":steering_sig_pin.read(),
+                "steeringAngleDeg":360 * (steering_sig_pin.read()-0.5),
+                "motorSpeed":self.speed,
+                "batteryStatus": 0.82, #hardcoded data.
+                "generalStatus": self.status
+                }
+        # print(info)
+        return info
+    def getFakeInfo(self):
+        
+        # sendInfo
+        info = {
+                "steeringAngleRaw":0.5,
+                "steeringAngleDeg":0,
+                "motorSpeed":0.42,
+                "batteryStatus": 0.82,
+                "generalStatus": "creeping forward"
                 }
         return info
-    
-    
+
+if __name__ == "__main__":
+    from time import sleep as delay
+    import csv
+    import cv2
+    import math
+
+    cap = cv2.VideoCapture(0)
+    car = Car()
+    n = 0
+    df = []
+    while 1:
+        where = input("press wasd:")
+        if where =='w':
+            car.forward(1)
+            delay(0.5)
+            car.stop()
+        if where =='a':
+            car.left()
+        if where =='s':
+            car.backward(1)
+            delay(0.5)
+            car.stop()
+        if where =='d':
+            car.right()
+        
+        ret, frame = cap.read()
+        
+        carInfo = car.getInfo()
+        carInfo = [carInfo['steeringAngleRaw'],carInfo['motorSpeed']]
+        print(carInfo)
+        
+        with open('dt.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(carInfo)
+           
+        filename = f"{n}_.jpg"
+        delay(0.1)
+        cv2.imwrite(filename, frame)
+        
+        n = n + 1
+        
